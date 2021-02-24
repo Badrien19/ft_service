90cda0d8eb66120511949cda15558e346a8a1747
arapaill <arapaill@student.s19.be>
2021-02-24 14:35:31 +0100

    starting the setup, added  clean.sh

---
+++ setup.sh
@@ -0,0 +1,21 @@
+echo "
+  __ _                           _               
+ / _| |_     ___  ___ _ ____   _(_) ___ ___  ___ 
+| |_| __|   / __|/ _ \ '__\ \ / / |/ __/ _ \/ __|
+|  _| |_    \__ \  __/ |   \ V /| | (_|  __/\__ \\
+|_|  \__|___|___/\___|_|    \_/ |_|\___\___||___/
+                               
+"
+echo "
+____start Minikube____
+"
+minikube start
+kubectl get po -A 
+echo "
+____app deployment____
+"
+kubectl create deployment hello-minikube --image=k8s.gcr.io/echoserver:1.4
+kubectl expose deployment hello-minikube --type=NodePort --port=8080
+kubectl get services hello-minikube
+minikube service hello-minikube
+kubectl port-forward service/hello-minikube 7080:8080
