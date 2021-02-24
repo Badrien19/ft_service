c02f093e11c0f0ebf34f0015d6b3896c0f591d56
CG_Walker <cogo97@hotmail.com>
2021-02-24 15:24:29 +0100

    Updated setup.sh & added metallb config files

--- setup.sh
+++ setup.sh
@@ -6,16 +6,13 @@ echo "
 |_|  \__|___|___/\___|_|    \_/ |_|\___\___||___/
                                
 "
-echo "
-____start Minikube____
-"
+echo "____Starting Minikube____"
+
 minikube start
-kubectl get po -A 
-echo "
-____app deployment____
-"
-kubectl create deployment hello-minikube --image=k8s.gcr.io/echoserver:1.4
-kubectl expose deployment hello-minikube --type=NodePort --port=8080
-kubectl get services hello-minikube
-minikube service hello-minikube
-kubectl port-forward service/hello-minikube 7080:8080
+# kubectl get po -A 
+
+echo "____Configuring Metallb____"
+
+# Metallb config:
+kubectl apply -f ./metallb/metallb.yaml
+kubectl create -f ./metallb/configmap.yaml
