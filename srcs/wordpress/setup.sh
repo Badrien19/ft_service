f487cf9676275ac4008bb0f380794a650f843297
arapaill <arapaill@student.s19.be>
2021-03-09 16:28:01 +0100

    wordpress added but service not working

--- srcs/wordpress/setup.sh
+++ srcs/wordpress/setup.sh
@@ -3,5 +3,6 @@ wget https://wordpress.org/latest.tar.gz
 tar -xzvf latest.tar.gz
 mv ./wordpress /var/www/localhost
 rm latest.tar.gz
+chown -R nginx:nginx /var/www/localhost/wordpress
 
 supervisord
