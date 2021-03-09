dd1243e41a2fc39767c88d9c423f8758fa4da00e
arapaill <arapaill@student.s19.be>
2021-03-09 17:07:25 +0100

    ssl added

--- srcs/wordpress/setup.sh
+++ srcs/wordpress/setup.sh
@@ -4,5 +4,5 @@ tar -xzvf latest.tar.gz
 mv ./wordpress /var/www/localhost
 rm latest.tar.gz
 chown -R nginx:nginx /var/www/localhost/wordpress
-
+openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/localhost.pem -keyout /etc/nginx/ssl/localhost.key -subj "/C=BE/ST=Bruxelles/L=Bruxelles/O=19 school/OU=Bourdanne squad/CN=localhost"
 supervisord
