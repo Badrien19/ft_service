1dedf22b1642038300a93a0db30b5c25181754b1
CG-Walker <cogo97@hotmail.com>
2021-04-28 20:14:12 +0200

    Résolu le fait que le yaml d'InfluxDB ne conservait pas les bonnes données

--- srcs/influxdb/setup.sh
+++ srcs/influxdb/setup.sh
@@ -5,32 +5,32 @@
 /usr/sbin/influxd & sleep 3
 
 # Initialize database
-influx -execute "CREATE DATABASE FTPS"
-influx -execute "CREATE USER admin WITH PASSWORD 'admin'"
+influx -execute "CREATE DATABASE IF NOT EXIST FTPS"
+influx -execute "CREATE USER IF NOT EXIST admin WITH PASSWORD 'admin'"
 influx -execute "GRANT ALL ON FTPS TO admin"
 
-influx -execute "CREATE DATABASE Grafana"
-influx -execute "CREATE USER admin WITH PASSWORD 'admin'"
+influx -execute "CREATE DATABASE IF NOT EXIST Grafana"
+influx -execute "CREATE USER IF NOT EXIST admin WITH PASSWORD 'admin'"
 influx -execute "GRANT ALL ON Grafana TO admin"
 
-influx -execute "CREATE DATABASE InfluxDB"
-influx -execute "CREATE USER admin WITH PASSWORD 'admin'"
+influx -execute "CREATE DATABASE IF NOT EXIST InfluxDB"
+influx -execute "CREATE USER IF NOT EXIST admin WITH PASSWORD 'admin'"
 influx -execute "GRANT ALL ON InfluxDB TO admin"
 
-influx -execute "CREATE DATABASE mysql"
-influx -execute "CREATE USER admin WITH PASSWORD 'admin'"
+influx -execute "CREATE DATABASE IF NOT EXIST mysql"
+influx -execute "CREATE USER IF NOT EXIST admin WITH PASSWORD 'admin'"
 influx -execute "GRANT ALL ON mysql TO admin"
 
-influx -execute "CREATE DATABASE nginx"
-influx -execute "CREATE USER admin WITH PASSWORD 'admin'"
+influx -execute "CREATE DATABASE IF NOT EXIST nginx"
+influx -execute "CREATE USER IF NOT EXIST admin WITH PASSWORD 'admin'"
 influx -execute "GRANT ALL ON nginx TO admin"
 
-influx -execute "CREATE DATABASE phpmyadmin"
-influx -execute "CREATE USER admin WITH PASSWORD 'admin'"
+influx -execute "CREATE DATABASE IF NOT EXIST phpmyadmin"
+influx -execute "CREATE USER IF NOT EXIST admin WITH PASSWORD 'admin'"
 influx -execute "GRANT ALL ON phpmyadmin TO admin"
 
-influx -execute "CREATE DATABASE Wordpress"
-influx -execute "CREATE USER admin WITH PASSWORD 'admin'"
+influx -execute "CREATE DATABASE IF NOT EXIST Wordpress"
+influx -execute "CREATE USER IF NOT EXIST admin WITH PASSWORD 'admin'"
 influx -execute "GRANT ALL ON Wordpress TO admin"
 
 # Keep container running
